64df14496f169202f6f403bcf2620d4a3a1757b9
jeeun99 <wpdms27@gmail.com>
2023-06-13 12:49:12 +0900

    about page

--- js/index.js
+++ js/index.js
@@ -1,14 +1,47 @@
 // toggle button js
 const $toggleBtn = document.querySelectorAll(".toggleSwitch");
 const $wrap = document.querySelector(".wrap");
+const $techIcons = document.querySelectorAll(".techIcons img");
 
 $toggleBtn.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     toggle.classList.toggle("active");
     $wrap.classList.toggle("on");
+
+    // techIcons
+    $techIcons.forEach((icons) => {
+      let alts = icons.getAttribute("alt");
+      if ($wrap.classList.contains("on")) {
+        icons.setAttribute("src", `./img/techIcons/${alts}Dark.png`);
+      } else {
+        icons.setAttribute("src", `./img/techIcons/${alts}Light.png`);
+      }
+      ``;
+    });
   });
 });
-
+// techIcons
+$techIcons.forEach((icons) => {
+  icons.addEventListener("mouseover", () => {
+    let alts = icons.getAttribute("alt");
+    icons.setAttribute("src", `./img/techIcons/${alts}.png`);
+  });
+  icons.addEventListener("mouseout", () => {
+    let alts = icons.getAttribute("alt");
+    if ($wrap.classList.contains("on")) {
+      icons.setAttribute("src", `./img/techIcons/${alts}Dark.png`);
+    } else {
+      icons.setAttribute("src", `./img/techIcons/${alts}Light.png`);
+    }
+  });
+  icons.addEventListener("click", () => {
+    $techIcons.forEach((icons) => {
+      icons.classList.remove("on");
+    });
+    icons.classList.add("on");
+  });
+});
+// ---------------------------------------------
 // changeText js
 const texts = [
   "효율적인 코드를 짜는",
@@ -27,7 +60,6 @@ function changeText() {
     changeTextElement.classList.remove("show");
   }, 1500);
 }
-
 setInterval(changeText, 2500);
 
 // ham button
